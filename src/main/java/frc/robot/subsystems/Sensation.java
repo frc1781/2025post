1aa9d84b6364968dbe61ac36ffebce9883e1c572
ian <irbrost@cps.edu>
2025-05-07 08:16:17 -0500

    caught it

--- src/main/java/frc/robot/subsystems/Sensation.java
+++ src/main/java/frc/robot/subsystems/Sensation.java
@@ -16,6 +16,9 @@ public class Sensation
     public Sensation()
     {
         enterBeam = new DigitalInput(Constants.SensationConstants.enter);
+        hopperBackBeam = new DigitalInput(Constants.SensationConstants.hopperBack);
+        hopperFrontBeam = new DigitalInput(Constants.SensationConstants.hopperFront);
+        exitBeam = new DigitalInput(Constants.SensationConstants.exit);
     }
 
     public BooleanSupplier enterSupplier() 
@@ -26,6 +29,30 @@ public class Sensation
             public boolean getAsBoolean() 
             {
                 return !enterBeam.get();
+            }
+        };
+    }
+
+    public BooleanSupplier hopperSupplier() 
+    {
+        return new BooleanSupplier()
+        {
+            @Override
+            public boolean getAsBoolean() 
+            {
+                return (!hopperBackBeam.get()) || (!hopperFrontBeam.get());
+            }
+        };
+    }
+
+    public BooleanSupplier exitSupplier() 
+    {
+        return new BooleanSupplier()
+        {
+            @Override
+            public boolean getAsBoolean() 
+            {
+                return !exitBeam.get();
             }
         };
     }
