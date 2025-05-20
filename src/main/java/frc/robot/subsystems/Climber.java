25de86f12c73e3e538d67d5ae943a3ac2514053f
BryceH127 <bkharris5@cps.edu>
2025-05-20 16:19:01 -0500

    Update Climber.java

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -9,13 +9,64 @@ import team1781.LinSparkMax;
 import java.util.function.BooleanSupplier;
 
 public class Climber extends SubsystemBase {
-    LinSparkMax motor;
+    private LinSparkMax motor;
+    boolean initalized; 
 
     public Climber() {
+        initalized = false;
         motor = new LinSparkMax(Constants.Climber.MOTOR);
     }
     
     public Command clearCoral(BooleanSupplier hasCoralToClear) {
         return new RunCommand(() -> {motor.set(hasCoralToClear.getAsBoolean() ? 0.5 : 0);}, this);
     }         
+
+    public Command descend(){
+        return this.runOnce(() -> {
+            if (!climbing) {
+                climberPID.reset();
+            }
+            climbing = true;
+            requestedPosition += 0.03}
+            );
+    }
+
+    public Command climb(){
+        return this.runOnce(() -> {
+            requestedPosition -= 0.03;
+
+            if (!climbing) {
+                climberPID.reset();
+            }
+            climbing = true;
+        });
+        
+    }
+
+    public Command run(){
+
+        return new RunCommand(() -> {
+            
+            if (initalized == false){
+                requestedPosition = armEncoder.getPosition();
+                initalized = true;
+            }
+
+            if (climbing) {
+                climberDutyCycle = climberPID.calculate(armEncoder.getPosition(), requestedPosition) + gravityDutyCycle;
+            }
+            else {
+                climberDutyCycle = 0.0;
+            }
+
+            climberDutyCycle = EEUtil.clamp(-0.5, 0.5, climberDutyCycle);
+            leverMotor.set(climberDutyCycle);
+            Logger.recordOutput("Climber/dutyCycle", climberDutyCycle);
+            Logger.recordOutput("Climber/position", armEncoder.getPosition());
+            Logger.recordOutput("Climber/requestedPosition", mMotorPosition);
+            
+        });
+    }
+
+
 }
