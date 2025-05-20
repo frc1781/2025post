9239ab26d3af00523b1e63aaf3e7b8bdbd499c65
BryceH127 <bkharris5@cps.edu>
2025-05-20 15:44:06 -0500

    Create Climber.java

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import team1781.LinSparkMax;
+import java.util.function.BooleanSupplier;
+
+public class Climber extends SubsystemBase {
+    LinSparkMax motor;
+
+    public Climber() {
+        motor = new LinSparkMax(Constants.Climber.MOTOR);
+    }
+    
+    public Command clearCoral(BooleanSupplier hasCoralToClear) {
+        return new RunCommand(() -> {motor.set(hasCoralToClear.getAsBoolean() ? 0.5 : 0);}, this);
+    }         
+}
