ef3773e4310635862ad6d2de67961649e3514fe4
Bryce <bkharris5@cps.edu>
2025-05-21 16:37:30 -0500

    Added climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,74 +1,83 @@
 package frc.robot.subsystems;
 
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 import java.util.function.BooleanSupplier;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import CRA.CRASparkMax;
 
 public class Climber extends SubsystemBase {
-    private LinSparkMax motor;
-    boolean initalized; 
 
-    public Climber() {
-        initalized = false;
-        motor = new LinSparkMax(Constants.Climber.MOTOR);
-    }
-    
-    public Command clearCoral(BooleanSupplier hasCoralToClear) {
-        return new RunCommand(() -> {motor.set(hasCoralToClear.getAsBoolean() ? 0.5 : 0);}, this);
-    }         
+    private RelativeEncoder armEncoder;
+    private double requestedPosition;
+    private double climberDutyCycle;
+    private PIDController climberPID;
+    private final double gravityDutyCycle = 0.0;
+    private SparkMax leverMotor;
+    private ArmFeedforward armFeedforward;
 
-    public Command descend(){
-        return this.runOnce(() -> {
-            if (!climbing) {
-                climberPID.reset();
-            }
-            climbing = true;
-            requestedPosition += 0.03}
-            );
-    }
+     public Climber() {
+        leverMotor = new SparkMax(Constants.Climber.MOTOR, SparkLowLevel.MotorType.kBrushless);
 
-    public Command climb(){
-        return this.runOnce(() -> {
-            requestedPosition -= 0.03;
+        SparkMaxConfig leverMotorConfig = new SparkMaxConfig();
+        leverMotorConfig.idleMode(SparkBaseConfig.IdleMode.kBrake);
+        leverMotorConfig.inverted(true);
+        leverMotorConfig.encoder.positionConversionFactor(Constants.Climber.RADIANS_PER_REVOLUTION);
+        leverMotorConfig.closedLoop.apply(Constants.Climber.CLOSED_LOOP_CONFIG);
 
-            if (!climbing) {
-                climberPID.reset();
-            }
-            climbing = true;
-        });
-        
+        leverMotor.configure(leverMotorConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+        armFeedforward = new ArmFeedforward(Constants.Climber.KS, Constants.Climber.KG, Constants.Climber.KV);
+        armEncoder = leverMotor.getEncoder();
+        requestedPosition = armEncoder.getPosition();
+        armFeedforward = new ArmFeedforward(Constants.Climber.KS, Constants.Climber.KG, Constants.Climber.KV);
+        climberDutyCycle = 0.0;
+        climberPID = new PIDController(2, 0, 0);
+        climberPID.reset();
     }
 
-    public Command run(){
+    Command idle(){
 
         return new RunCommand(() -> {
-            
-            if (initalized == false){
-                requestedPosition = armEncoder.getPosition();
-                initalized = true;
-            }
-
-            if (climbing) {
-                climberDutyCycle = climberPID.calculate(armEncoder.getPosition(), requestedPosition) + gravityDutyCycle;
-            }
-            else {
-                climberDutyCycle = 0.0;
-            }
-
-            climberDutyCycle = EEUtil.clamp(-0.5, 0.5, climberDutyCycle);
+            climberDutyCycle = climberPID.calculate(armEncoder.getPosition(), requestedPosition) + gravityDutyCycle;
+           
+            climberDutyCycle = Math.min(0.1, climberDutyCycle);
+            climberDutyCycle = Math.max(-0.1, climberDutyCycle);
             leverMotor.set(climberDutyCycle);
-            Logger.recordOutput("Climber/dutyCycle", climberDutyCycle);
-            Logger.recordOutput("Climber/position", armEncoder.getPosition());
-            Logger.recordOutput("Climber/requestedPosition", mMotorPosition);
-            
-        });
+        }, this);
+
     }
 
+    Command ascend(){
+        return new InstantCommand(() -> {
+            requestedPosition -= .03;
+        }, this);
+    }
+
+    Command descend(){
+        return new InstantCommand(() -> {
+            requestedPosition += .03;
+        }, this);
+    }
+
+    Command resetPID(){
+        return new InstantCommand(() -> {
+            climberPID.reset();
+        },this);
+    }
 
 }
