2797ebdb98ac3b22e2463af1c93bcf493f8dfab8
Keith Van Rhein <irbrost@cps.edu>
2025-05-21 08:51:23 -0500

    First idea for CRA utility classes

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -5,11 +5,13 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import team1781.LinSparkMax;
+
 import java.util.function.BooleanSupplier;
 
+import CRA.CRASparkMax;
+
 public class Climber extends SubsystemBase {
-    LinSparkMax motor;
+    CRASparkMax motor;
 
     // public Climber() {
     //     motor = new LinSparkMax(Constants.Climber.MOTOR);
