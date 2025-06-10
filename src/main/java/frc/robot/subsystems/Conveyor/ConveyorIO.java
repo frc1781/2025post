6359fbd7ac20151c2f04a0162baed1e8aae17d3d
ian <irbrost@cps.edu>
2025-06-10 08:16:35 -0500

    ok ill see myself out

--- src/main/java/frc/robot/subsystems/Conveyor/ConveyorIO.java
+++ src/main/java/frc/robot/subsystems/Conveyor/ConveyorIO.java
@@ -2,5 +2,9 @@ package frc.robot.subsystems.Conveyor;
 
 public interface ConveyorIO 
 {
-    
+    void set(float speed);
+    void brake(boolean value);
+    void inverted(boolean value);
+
+    void update();
 }
